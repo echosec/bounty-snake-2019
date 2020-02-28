5ab4e269e4dfc8f2015210ebecab7b97ae0b23a2
Brandon Brown <brandonb@echosec.net>
2020-02-27 16:10:11 -0800

    Revert tf changes as bart will address in another PR

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,20 +15,22 @@ provider "digitalocean" {
   token = var.api_token
 }
 
+resource "digitalocean_ssh_key" "bounty_snake_ssh_key" {
+  name       = "bounty-snake-ssh-key"
+  public_key = var.droplet_ssh_key
+}
+
 resource "digitalocean_droplet" "bounty_snake_droplet" {
   image     = "docker-18-04"
   name      = "echosec-bounty-snake"
   region    = "nyc3"
   size      = "s-2vcpu-4gb"
-  ssh_keys  = [
-    25059594 # brandonb@echosec.net
-  ]
+  ssh_keys  = [digitalocean_ssh_key.bounty_snake_ssh_key.fingerprint]
   user_data = <<EOM
     #cloud-config
     runcmd:
-      - docker login docker.pkg.github.com -u ${var.github_username} -p ${var.github_token}
       - docker pull docker.pkg.github.com/echosec/bounty-snake-2020/bounty-snake-2020:latest
-      - docker run -t -d -p 80:5000 docker.pkg.github.com/echosec/bounty-snake-2020/bounty-snake-2020:latest
+      - docker run -t -d -p 80:5000 docker.pkg.github.com/echosec/bounty-snake-2020/bounty-snake-2020
     EOM
 }
 
@@ -49,21 +51,15 @@ resource "digitalocean_firewall" "bounty_snake_firewall" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
   outbound_rule {
     protocol              = "tcp"
-    port_range            = "53"
+    port_range            = "22"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
-    protocol              = "udp"
-    port_range            = "53"
+    protocol              = "tcp"
+    port_range            = "80"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,12 +3,7 @@ variable "api_token" {
   type        = string
 }
 
-variable "github_username" {
-  description = "GitHub API username to login to the registry as"
-  type        = string
-}
-
-variable "github_token" {
-  description = "GitHub personal access token to login to registry with"
+variable "droplet_ssh_key" {
+  description = "Public SSH key needed to SSH into DigitalOcean droplet"
   type        = string
 }

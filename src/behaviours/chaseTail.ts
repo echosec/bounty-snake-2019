aed35e6e39bff2bcaf8272cdb84d6cde6b3405aa
Mike Baart <mbaart@echosec.net>
2020-03-06 15:01:39 -0800

    Refactor function to eliminate initialization of new Pathfinder class

--- src/behaviours/chaseTail.ts
+++ src/behaviours/chaseTail.ts
@@ -1,4 +1,4 @@
-import { ISnake, IBoard, ICoordinate, Directions } from '../Types';
+import { ISnake, ICoordinate, Directions } from '../Types';
 import Pathfinder from '../Pathfinder';
 
 /**
@@ -6,10 +6,7 @@ import Pathfinder from '../Pathfinder';
  * @param {ISnake} us - our snake
  * @returns {Directions} returns the next direction
  */
-export const chaseTail = (board: IBoard, us: ISnake): Directions => {
-  const snakes: ISnake[] = board.snakes;
-  const PF = new Pathfinder(board, snakes);
-
+export const chaseTail = (PF: Pathfinder, us: ISnake): Directions => {
   const head: ICoordinate = us.body[0];
   const tail: ICoordinate = us.body[us.body.length - 1];
 
@@ -17,3 +14,4 @@ export const chaseTail = (board: IBoard, us: ISnake): Directions => {
 };
 
 export default chaseTail;
+

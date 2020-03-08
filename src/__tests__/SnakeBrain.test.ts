5e3ed0e573ede198011ac530a6dc376f2623ecbe
shaun-mac <shaunmacp@gmail.com>
2020-03-07 19:32:20 -0800

    Add test for giveUp

--- src/__tests__/SnakeBrain.test.ts
+++ src/__tests__/SnakeBrain.test.ts
@@ -7,8 +7,17 @@ describe('GameState Class Tests', () => {
   test('Class creation and default move', () => {
     // Arrange & Act
     const mockGameStateObject = getGameStateFromMock(headAndTailSnake1);
-    const direction = new SnakeBrain(mockGameStateObject).decide().act();
+    const direction = new SnakeBrain(mockGameStateObject).decide(mockGameStateObject, false).act();
     // Assert
     expect(direction).toBe(Directions.LEFT);
+  });
+
+  test('Call turtle when giveUp = true is passed to decide', () => {
+    // Arrange & Act
+    const mockGameStateObject = getGameStateFromMock(headAndTailSnake1);
+    const direction = new SnakeBrain(mockGameStateObject).decide(mockGameStateObject, true).act();
+
+    // Assert
+    expect(direction).toBe(Directions.DOWN);
   });
 });

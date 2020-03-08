1b5b873d7c52a1c208add7a556167fbe0ec6f65c
Brandon Brown <brandonb@echosec.net>
2020-03-08 00:04:12 -0800

    Open port 80 on firewall so the droplet can run updates and installs using apt

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,6 +78,12 @@ resource "digitalocean_firewall" "bounty_snake_firewall" {
 
   outbound_rule {
     protocol              = "tcp"
+    port_range            = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
     port_range            = "443"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
